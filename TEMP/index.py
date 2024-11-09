7867de8cc4efbf6d92c5759cfaae696e57be0752
PAWAN BISHNOI <knowledgetechnology3@gmail.com>
2024-11-09 11:06:39 +0530

    Update index.py

--- TEMP/index.py
+++ TEMP/index.py
@@ -7,9 +7,15 @@ def print_user_details(full_name, age, number):
 
 
 # Get inputs for full name, age, and phone number, and pass them to the print_user_details function
-full_name = input("Enter Your Full Name: ")
 age = int(input("Enter Your Age: "))  # Convert age to integer
 phone_number = int(input("Enter Your Number: "))  # Convert phone number to integer
 
 # Call the function to display user details
+
+while True:
+    full_name = input("Enter Your Full Name: ")
+    if(full_name==full_name.isalpha()):
+        print("Write Name")
+    else:
+        print("Wrong Input Please Enter Only Name")
 print_user_details(full_name, age, phone_number)
